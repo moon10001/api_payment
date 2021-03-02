fc3fc2a2b2aeb43670aaee831a27d90d0c3fa36d
Martin Yohansa <martin.yohansa@gmail.com>
2021-03-02 23:09:58 +0700

    Added details to report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -20,20 +20,56 @@ class ReportController extends Controller
       DB::enableQueryLog();
       $startYear = $request->year;
       $endYear = intval($startYear) + 1;
-      $periode = [];
-      for ($i = 1; $i <= 12; $i++) {
-        if ($i < 7 ) {
-          array_push($periode, $i.$endYear);
-        } else {
-          array_push($periode, $i.$startYear);
-        }
-      }
-      $results = DB::table('tr_invoices')
-      ->select('*')
+      $periode = [
+        '07'.$startYear,
+        '08'.$startYear,
+        '09'.$startYear,
+        '10'.$startYear,
+        '11'.$startYear,
+        '12'.$startYear,
+        '01'.$endYear,
+        '02'.$endYear,
+        '03'.$endYear,
+        '04'.$endYear,
+        '05'.$endYear,
+        '06'.$endYear,
+      ];
+
+      $student = DB::table('ms_temp_siswa')
+      ->select('id', 'name', 'class', 'paralel', 'jurusan')
+      ->where('id', $request->va_code)
+      ->first();
+
+      $invoices = DB::table('tr_invoices')
+      ->select(
+        '*',
+        DB::raw('SUBSTR(periode, 1, 2) as periode_month'),
+        DB::raw('DATE_FORMAT(payments_date, "%d-%m-%Y") as payments_date')
+      )
       ->whereIn('periode', $periode)
       ->where('temps_id', $request->va_code)
       ->get();
-      var_dump(DB::getQueryLog());
-      return response()->json($results);
+
+      $lastInvoice= $invoices->whereNull('payments_date')->first();
+
+      $liabilities = [];
+      if ($lastInvoice) {
+        $liabilities = DB::table('tr_payment_details')
+        ->select(
+          'tr_payment_details.invoices_id',
+          'tr_payment_details.periode',
+          'tr_payment_details.nominal',
+          'prm_payments.name'
+          )
+        ->join('prm_payments', 'tr_payment_details.payments_id', 'prm_payments.id')
+        ->where('invoices_id', $lastInvoice->id)
+        ->get();
+      }
+
+      return response()->json([
+        'student' => $student,
+        'invoices' => $invoices,
+        'liabilities' => $liabilities,
+      ]);
     }
 }
