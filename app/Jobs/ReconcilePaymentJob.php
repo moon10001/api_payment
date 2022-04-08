956a70027b9481e0fad3ec6c6dacc19c81fd99b6
Martin Yohansa <martin.yohansa@gmail.com>
2022-04-08 14:49:24 +0700

    Fixing reconcile payment job

--- app/Jobs/ReconcilePaymentJob.php
+++ app/Jobs/ReconcilePaymentJob.php
@@ -22,12 +22,13 @@ class ReconcilePaymentJob extends Job
     public function handle()
     {
       try {
-        $date = date("Y-m-d");
+        //$date = date("Y-m-d");
+        $date = date("2021-12-05");
         $unitsVA = Cache::get('prm_va', function() {
           $res = DB::table('prm_va')->get();
           return $res;
         });
-        
+
 	    DB::enableQueryLog();
         $transactions = DB::table('mt940')
         ->select(
@@ -41,20 +42,20 @@ class ReconcilePaymentJob extends Job
           DB::raw('NOW()')
         )
         ->join('prm_va', function($join) {
-          $join->on('mt940.va', 'like', DB::raw('CONCAT(prm_va.va_code, "%")')); 
+          $join->on('mt940.va', 'like', DB::raw('CONCAT(prm_va.va_code, "%")'));
         })
 	    ->join('tr_payment_details', function($join) {
           $join->on('tr_payment_details.invoices_id', '=', DB::raw('CONCAT("INV-", mt940.temps_id, DATE_FORMAT(STR_TO_DATE(mt940.periode_from, "%m%y"), "%y%m"))'));
        	})
         ->join('prm_payments', 'tr_payment_details.payments_id', 'prm_payments.id')
+        ->whereRaw('(tr_payment_details.periode = CAST(periode_from as UNSIGNED) OR tr_payment_details.periode = CAST(periode_to as UNSIGNED))')
         ->whereRaw('DATE(mt940.created_at) = ?', $date)
-        ->groupBy('prm_va.va_code', 'mt940.payment_date', 'prm_payments.id')
-        ->orderBy('mt940.payment_date', 'ASC');
-        
-        DB::table('daily_reconciled_reports')->insertUsing([
-        	'payment_date', 'prm_payments_id', 'units_id', 'nominal', 'created_at', 'updated_at'
-        ], $transactions);
-        /*
+        ->groupBy('mt940.payment_date', 'mt940.va', 'prm_payments.name', 'prm_payments.id')
+        ->orderBy('mt940.payment_date', 'ASC')
+        ->orderBy('mt940.va', 'ASC')
+        ->orderBy('tr_payment_details.periode', 'ASC')
+        ->get();
+
         foreach($unitsVA as $va) {
           if (empty($va->va_code)) {
             continue;
