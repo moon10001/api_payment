ffb35c922c754dc67434e6bb804ecc555217282d
spindev <udos1001001@gmail.com>
2022-04-19 16:09:19 +0700

    Fixing Seeder

--- database/seeders/SeedH2H.php
+++ database/seeders/SeedH2H.php
@@ -18,7 +18,7 @@ class SeedH2H extends Seeder
      */
     public function run()
     {
-        $begin = new DateTime('2021-01-01');
+        $begin = new DateTime('2021-03-01');
         $end = new DateTime('2021-12-31');
 
         $interval = DateInterval::createFromDateString('1 day');
