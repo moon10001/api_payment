95959b6cd94bc01f696ec727f8c402885db88973
Martin Yohansa <martin.yohansa@gmail.com>
2022-08-01 14:06:57 +0700

    Renaming controller classname

--- app/Http/Controllers/SupervisionsController.php
+++ app/Http/Controllers/SupervisionsController.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
-class ReportController extends Controller
+class SupervisionsController extends Controller
 {
     /**
      * Create a new controller instance.
